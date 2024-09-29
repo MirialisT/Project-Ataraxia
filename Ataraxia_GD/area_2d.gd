8ad3f3f96c26b60b036faa5e063af5e4c214832e
MirialisT <mirialisT@protonmail.com>
2024-09-29 16:19:19 +0300

    auto-load-main

---
+++ Ataraxia_GD/area_2d.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+@export var tile_size = 64
+var inputs = {"ui_right": Vector2.RIGHT,
+			"ui_left": Vector2.LEFT,
+			"ui_up": Vector2.UP,
+			"ui_down": Vector2.DOWN}
+			
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	position = position.snapped(Vector2.ONE * tile_size)
+	position += Vector2.ONE * tile_size/2
+
+
+func _unhandled_input(event):
+	for dir in inputs.keys():
+		if event.is_action_pressed(dir):
+			move(dir)
+
+func move(dir):
+	position += inputs[dir] * tile_size
