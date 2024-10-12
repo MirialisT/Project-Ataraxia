f8ef145645d8cfe442ed695d44352ffd8cdf80b2
MirialisT <mirialisT@protonmail.com>
2024-10-12 18:52:41 +0300

    auto-load-main

--- Ataraxia_GD/Scripts/GlobalScripts/time_processer.gd
+++ Ataraxia_GD/Scripts/GlobalScripts/time_processer.gd
@@ -3,6 +3,7 @@ signal process_time(time_amount: int)
 signal bleed_on_time()
 @export var start_time: int
 # store time from last time visited, check DNPC script
+# write new functions, fix this shit
 func DEBUG_LOG(int_time: int):
 	print("DEBUG::increase_time_by_%d" % int_time)
 	process_time.emit(5)
